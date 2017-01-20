2c241501a1c978d102772cce35e66dac306de7c6
Robin Ole Heinemann <robin.ole.heinemann@t-online.de>
2017-01-20 12:35:47 +0100

    update UB

---
+++ theo1/tutorial/login.php
@@ -0,0 +1,159 @@
+<!DOCTYPE HTML>
+
+<html lang = "DE" >
+<head>
+<title>&Uuml;bungsgruppenverwaltung der Fakult&auml;t für Physik und Astronomie</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-15"><meta name="keywords" content="">
+<meta name="description" content="">
+<meta http-equiv="Content-Language" content="de">
+<meta name="author" content="">
+<meta name="organization-name" content="Kirchhoff-Institut f&uuml;r Physik">
+<meta name="organization-email" content="info@kip.uni-heidelberg.de">
+<meta name="city" content="Heidelberg">
+<meta name="country" content="Germany - Deutschland">
+<meta name="language" content="German, Deutsch, de, at,ch">
+<meta name="robots" content="index">
+<meta name="robots" content="follow">
+<meta name="revisit-after" content="3 days">
+<meta http-equiv="imagetoolbar" content="no">
+<meta name="MSSmartTagsPreventParsing" content="true">
+<link rel="shortcut icon" HREF="/favicon.ico">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style type="text/css">
+@import url(/design/css/uni_layout/css/flexible-grids.css);
+@import url(/design/css/uni_layout/css/uni_hd_basemod.css);
+</style>
+<!--[if lte IE 7]>
+<link href="/design/css/uni_layout/css/yaml4/yaml/core/iehacks.css" rel="stylesheet" type="text/css">
+<![endif]-->
+<style type="text/css">
+@import url(/design/css/uni_layout/css/uni_hd_style_mod.css);
+@import url(/design/css/urz.css);
+</style>
+
+<style type="text/css" media="print">
+     @import url(/design/css/localstyles.css);
+     @import url(/design/css/print.css);
+     @import url(/css/uebungen.css);
+     @import url(/css/printuebungen.css);
+</style>
+<style type="text/css" media="screen">
+     @import url(/design/css/localstyles.css);
+     @import url(/design/css/localscreenstyles.css);
+     @import url(/design/css/screen.css);
+     @import url(/css/uebungen.css);
+     @import url(/css/screenuebungen.css);
+</style>
+</head>
+<body class="hidecol2"  onload='setFocus()'>
+<div id="topBarInstitute">
+<div id="sprungmarken">
+<a name="up" id="up"></a>
+<ul class="ym-skiplinks">
+	<li><a class="ym-skip" href="#nav">Navigation</a></li>
+	<li>|</li>
+	<li><a class="ym-skip" href="#mitte">Inhalt</a></li>
+</ul>
+</div> <!-- sprungmarken -->
+</div> <!-- topBarInstitute -->
+<div id="main">
+<header>
+<div class="ym-cbox">
+<div id="universitaet">
+<a href="http://www.uni-heidelberg.de/">
+<img
+src="/design/images/uni/schiftzug_mit_siegel.gif"
+alt="Ruprecht-Karls-Universit&auml;t Heidelberg">
+</a>
+</div>
+<div id="greyBar"></div>
+</div>
+</header>
+<div class="ym-cbox">
+<div class="ym-grid linearize-level-1 ym-equalize">
+<!--  col 1  -->
+<div class="ym-col1 ym-cbox">
+<div id="col1_content">
+<div class="institutslogo"><div class="institutslogo"><a href='/uebungen/logo.php'><img src='/Phue2.jpg' alt='Fakult&auml;t f&uuml;r Physik und Astronomie' title='Neues Logo: PH&Uuml; - PHysik &Uuml;bungsgruppen'></div></div>
+<a id="nav" name="nav"></a>
+<div id='naviMainContainer'>
+
+  <ul>
+<li><a href='http://www.physik.uni-heidelberg.de/' >Fakult&auml;t</a></li>
+<li><a href='/uebungen/liste.php?onlyberid=0' >&Uuml;bungsgruppen</a></li>
+<li><a href='/uebungen/ergebnisse.php' >Ergebnisse</a></li>
+<li><a href='/uebungen/select.php' >Studium</a></li>
+<li><a href='/uebungen/admin/index.php' >Administration</a></li>
+<li><a href='/vorlesung/' >Vorlesungen</a></li>
+<li class="lilastNav"></li>
+   </ul>
+  
+ 
+
+</div>
+</div>
+</div>
+<!--  /col 1  -->
+<!--  col 3  -->
+<div class="ym-col3 ym-cbox">
+<a id="mitte" name="mitte"></a>
+<!--// Beginn content //-->
+<div id="col3_content">
+<div id="naviHor"> <a href='http://lsf.uni-heidelberg.de/'>LSF</a>
+	   | <a href='/uebungen/login.php?lang=en'>English</a></div>
+<div class="pathway"><a href='http://www.physik.uni-heidelberg.de'>Fakult&auml;t</a>
+	 &gt; <a href='/uebungen/'>&Uuml;bungen</a></div>
+<div>
+<div id="start">
+<h1>Anmeldeportal</h1>
+<br><div style='margin: 30px 20px 50px 50px'>
+<h3>Bitte einloggen:</h3><br>
+
+<script language='JavaScript' type='text/javascript'>
+//<!--
+function setFocus()
+{
+    document.forms[0].elements['username'].focus();
+}
+//-->
+</script>
+<br><br>
+   <form action='/uebungen/login.php' name='loginform' method='post' autocomplete='off'>
+    <table>
+      <tr>
+         <td align='right' class='feldname'>Benutzername :</td>
+	 <td align='left'><input type='text' name='username' length='10' value=''></td>
+      </tr>
+      
+      <tr>
+         <td align='center' colspan=2>&nbsp;</td>
+      </tr>
+      <tr>
+         <td align='center'>&nbsp;</td>
+         <td align='left'><input type='submit' name='submit' value=' senden '></td>
+      </tr>
+    </table>
+   </form>
+<br><div style='margin:30px 20px;padding:10px;background:#eee'>Bitte beachten Sie, dass der Login nur funktioniert, wenn
+Sie Cookies zulassen.</div></div>
+<div class='topVariabel'><a href='#up'><img alt='zum Seitenanfang' src='/design/images/icon_top.gif'></a></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<footer>
+<div id='footerBar'></div> <!--// roter Balken Seitenfu&szlig; //-->
+<div id='footer'>
+<div class='copyright'>
+&copy; Copyright Universit&auml;t Heidelberg.
+<a href='http://www.uni-heidelberg.de/impressum.html'>Impressum</a>.
+</div><br>
+</div> <!-- Ende footer -->
+</footer>
+</div>
+<script src='/design/css/uni_layout/css/yaml4/yaml/core/js/yaml-focusfix.js'></script>
+<div id='statistik'>
+</div>
+</body>
+</html> 
